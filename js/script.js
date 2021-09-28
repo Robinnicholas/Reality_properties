c1dc2ed3f63a337fd0e9de27fc7abeca62757164
Robinnicholas <robinnicholas2000@gmail.com>
2021-09-28 17:45:58 +0530

    new changes

--- js/script.js
+++ js/script.js
@@ -1,24 +1,56 @@
+// dropdown
+let clickableParagraph = document.querySelector("p.select");
+let clickableImg = document.querySelector(".dropdown-img");
+let dropdownContent = document.querySelector(".dropdown-content");
+
+clickableParagraph.addEventListener("click", () => {
+  dropdown();
+});
+
+clickableImg.addEventListener("click", () => {
+  dropdown();
+});
+
+document.addEventListener("click", (e) => {
+    if(e.target !== clickableParagraph && e.target !== clickableImg){
+        dropdownContent.style.display = "none";
+    }
+});
+
+function dropdown() {
+  dropdownContent.style.display = "block";
+  let dropList = document.querySelectorAll(".dropdown-content p");
+  dropList.forEach((e) => {
+    e.addEventListener("click", () => {
+      let selectedValue = e.innerHTML;
+      console.log(selectedValue);
+      clickableParagraph.innerHTML = selectedValue;
+      dropdownContent.style.display = "none";
+    });
+  });
+}
+
 // Owlcarousel
-$(document).ready(function(){
+$(document).ready(function () {
   $(".owl-carousel").owlCarousel({
-  	loop:true,
-    margin:10,
-    nav:true,
+    loop: true,
+    margin: 10,
+    nav: true,
     center: true,
     navText: [
-	    "<i class='fa fa-angle-left'></i>",
-	    "<i class='fa fa-angle-right'></i>"
-	],
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:3
-        }
-    }
+      "<i class='fa fa-angle-left'></i>",
+      "<i class='fa fa-angle-right'></i>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 3,
+      },
+    },
   });
 });
